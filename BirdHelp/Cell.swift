8c6775e816fa3975ffd8174f087a6ac59fed911c
GregD974 <38004510@co.univ-reunion.fr>
2021-04-06 17:51:41 +0200

    Ajout d'un button ,  et ajout de tableView / cell et 3 label

---
+++ BirdHelp/Cell.swift
@@ -0,0 +1,16 @@
+//
+//  Cell.swift
+//  BirdHelp
+//
+//  Created by user196294 on 4/5/21.
+//
+
+import UIKit
+//Permet d'acceder au éléments de la cellule 
+class Cell: UITableViewCell {
+    
+    @IBOutlet var imageViewBird: UIImageView! // 
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+}

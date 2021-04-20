1d8ff39ea0fcf9a5c6ce26325a4eaf42e8e33e7d
GregD974 <38004510@co.univ-reunion.fr>
2021-04-20 18:46:58 +0200

    background + image + advancer sur le BG

--- BirdHelp/GameScene.swift
+++ BirdHelp/GameScene.swift
@@ -16,13 +16,12 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     private var bird : SKSpriteNode! //commentaire a mettre
     private var timer : Timer?
+    private var background : SKSpriteNode!
+    private var floor1: SKSpriteNode!
+    private var floor2: SKSpriteNode!
   
     override func didMove(to view: SKView) {
         //commentaire a mettre
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
-            NotificationCenter.default.post(name: HeartStackView.enemyCollisionNotification, object: nil)
-            NotificationCenter.default.post(name: ScoreLabel.scoreNotification, object: nil)
-        })
         NotificationCenter.default.addObserver(forName: notifAppGameOver, object: nil, queue: .main) { _ in
             guard let vController = self.gameViewController.storyboard?.instantiateViewController(identifier: "gameover") else {
                 return
@@ -35,9 +34,11 @@ class GameScene: SKScene {
         gameViewController.scoreLabel.text = String(scoreInt) // commentaire a mettre
         
         self.bird = (childNode(withName:"//Bird") as! SKSpriteNode) // Commentaire a mettre
-        
+        self.background = (childNode(withName:"//bg") as! SKSpriteNode)
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        self.floor1 = self.childNode(withName: "//flor1") as? SKSpriteNode
+        self.floor2 = self.childNode(withName: "//flor2") as? SKSpriteNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
@@ -107,22 +108,27 @@ class GameScene: SKScene {
     }
     //fonction utiliser pour quand on reste appuyer sur le bouton cela répète le mouvement
     func didTapLeftForever(){
-        bird.run(.repeatForever(SKAction.move(by: CGVector.init(dx: -15, dy: 0), duration: 0.1)))
+        background.run(.repeatForever(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1)))
     }
     //fonction utiliser pour quand il arrête d'appuyer cela arrete le mouvement dans tout les cas
     func didTapLeftTouchUp(){
-        bird.removeAllActions()
+        background.removeAllActions()
     }    
     func  didTapRight() {
-        bird.run(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1))
-        
+        background.run(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1))
+        floor1.run(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1))
+        floor2.run(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1))
     }
     func didTapRightForever(){
-        bird.run(.repeatForever(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1)))
-    }    //comment
+        background.run(.repeatForever(SKAction.move(by: CGVector.init(dx: -15, dy: 0), duration: 0.1)))
+            floor1.run(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1))
+    floor2.run(SKAction.move(by: CGVector.init(dx: 15, dy: 0), duration: 0.1))
+    }
     
     func didTapRightTouchUp(){
-        bird.removeAllActions()
+        background.removeAllActions()
+        floor1.removeAllActions()
+        floor2.removeAllActions()
         
     }
     
